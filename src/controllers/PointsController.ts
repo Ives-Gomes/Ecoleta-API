c0782113dd634cd21561197f8da8f1059309ad96
Ives <ivesmoreira3@gmail.com>
2020-06-05 18:56:07 -0300

    New Database and IP Update

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -19,7 +19,7 @@ class PointsController {
 
     const serializedPoints = points.map((point) => {
       return {
-        ...points,
+        ...point,
         image_url: `http://192.168.0.9:3333/uploads/${point.image}`,
       };
     });
